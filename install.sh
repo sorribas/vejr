6b45bfb2ee123dd6454eccd8b9c8d06f5baddf1b
Eduardo Sorribas <eduardo@sorribas.org>
2020-05-17 20:46:29 +0200

    Fix install script.

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ esac
 
 url=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/sorribas/vejr/releases/latest) 
 url="${url##*/}"
-$SUDO_MAYBE curl -L -o /usr/local/bin/vejr  https://github.com/sorribas/vejr/releases/download/${url}/vejr-linux
+$SUDO_MAYBE curl -L -o /usr/local/bin/vejr  https://github.com/sorribas/vejr/releases/download/${url}/vejr-${os}
 $SUDO_MAYBE chmod +x /usr/local/bin/vejr
